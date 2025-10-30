43ce5d8373ccb7b3b08523e2ca9c7e335b234696
VirtualGack <virtualgack@gmail.com>
2025-10-30 18:07:58 +1100

    started to build GUI, will research grid

--- GorkLogger.py
+++ GorkLogger.py
@@ -3,17 +3,37 @@ import datetime
 
 root = tk.Tk()
 
-root.geometry("500x250")
+### screen config ###
+
+root.geometry("550x250")
 root.title("GorkLogger")
 root.config(bg="#121210")
 
-now = datetime.datetime.now()
-print(now)
+### set variables ###
 
-date = datetime.date.today()
-print(date)
+now = datetime.datetime.now() #sets the current date and time when calld.
+#print(now)
 
-file_title = f'call_log_{date}'
-print(file_title)
+current_time = now.strftime("%H:%M:%S") #seperates the time from now into HH:MM:SS.
+# print(current_time)
+
+date = datetime.date.today() # seperates the date from now.
+# print(date)
+
+file_title = f'call_log_{date}' #will be used to set the name of the txt file.
+# print(file_title)
+
+### main script ###
+
+caller_question = tk.Label(root, text="Caller =>", font=("Helvetica", 18) )
+caller_question.grid(row= 0, column= 0, pady=5, padx=10)
+
+caller_answer = tk.Entry(root, font=("Helvetica", 18), width=31)
+caller_answer.grid(row= 0, column= 1, pady=5, padx= 10)
+
+notes = tk.Text(root, font=("Helvetica", 15), height= 5)
+notes.grid(row= 1, column= 0)
+
+
 
 root.mainloop()

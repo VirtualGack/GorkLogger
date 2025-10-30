183bf062a101a2cb7ef7a6eb181bdcec97d2b84e
VirtualGack <virtualgack@gmail.com>
2025-10-30 21:41:30 +1100

    GUI updated, added button to end call

--- GorkLogger.py
+++ GorkLogger.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+import tkinter.font as tkFont
 import datetime
 
 root = tk.Tk()
 
 ### screen config ###
 
-root.geometry("550x250")
+root.geometry("500x250")
 root.title("GorkLogger")
-root.config(bg="#121210")
+root.config(bg="#09330d")
+root.resizable(False, False)
 
 ### set variables ###
 
@@ -23,16 +25,32 @@ date = datetime.date.today() # seperates the date from now.
 file_title = f'call_log_{date}' #will be used to set the name of the txt file.
 # print(file_title)
 
-### main script ###
+font = tkFont.Font(family="Comfortaa", size=18)
 
-caller_question = tk.Label(root, text="Caller =>", font=("Helvetica", 18) )
-caller_question.grid(row= 0, column= 0, pady=5, padx=10)
+### GUI layout ###
 
-caller_answer = tk.Entry(root, font=("Helvetica", 18), width=31)
-caller_answer.grid(row= 0, column= 1, pady=5, padx= 10)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-notes = tk.Text(root, font=("Helvetica", 15), height= 5)
-notes.grid(row= 1, column= 0)
+frame = tk.Frame(root, bg="#09330d")
+frame.grid(row= 0, column= 0, sticky="nsew")
+
+frame.columnconfigure(0, weight= 1)
+frame.columnconfigure(1, weight= 1)
+frame.rowconfigure(0, weight= 1)
+frame.rowconfigure(1, weight=4)
+
+caller_question = tk.Label(frame, text="Caller =>",  bg="#9cbda1", font=font ) #font=("Helvetica", 18)
+caller_question.grid(row= 0, column= 0, sticky="nsew", pady=5, padx=5)
+
+caller_answer = tk.Entry(frame, bg="#f7f7e8", fg="#09330d", font=font) #width=31 font=("Helvetica", 18)
+caller_answer.grid(row= 0, column= 1, sticky="nsew", pady=5, padx= 10)
+
+notes = tk.Text(root,  height=8, bg="#f7f7e8", fg="#09330d", font=font) #font=("Helvetica", 17)
+notes.grid(row= 1, column= 0, columnspan=2, sticky="nsew")
+
+submit_btn = tk.Button(root, text="End Call", font=font, bg="#09330D", fg="#9cbda1")
+submit_btn.place(rely=1.0, relx=1.0, anchor=tk.SE)
 
 
 

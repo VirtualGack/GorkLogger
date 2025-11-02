81b4bcdf08c37c0a339be84310391f8974206bd1
VirtualGack <virtualgack@gmail.com>
2025-11-02 12:10:14 +1100

    fixed missing button in last commit and added build file

--- GorkLogger.py
+++ GorkLogger.py
@@ -6,43 +6,36 @@ root = tk.Tk()
 
 ### screen config ###
 
-root.geometry("500x250")
-root.title("GorkLogger")
-root.config(bg="#09330d")
-root.resizable(False, False)
+root.geometry("500x250") # sets screen size
+root.title("GorkLogger") # sets the title of the app
+root.config(bg="#09330d") #sets the root background colour
+root.resizable(False, False) #prevents wuindow from being resized
 
 ### set variables ###
 
 now = datetime.datetime.now() #sets the current date and time when calld.
-#print(now)
-
 current_time = now.strftime("%H:%M:%S") #seperates the time from now into HH:MM:SS.
-# print(current_time)
-
 date = datetime.date.today() # seperates the date from now.
-# print(date)
-
-file_title = f'call_log_{date}' #will be used to set the name of the txt file.
-# print(file_title)
-
-font = tkFont.Font(family="Comfortaa", size=18)
-
-callerid = ""
+file_title = f'call_log_{date}.txt' #will be used to set the name of the txt file.
+font = tkFont.Font(family="Comfortaa", size=18) #sets the font uniform across the app
+callerid = "" #sets callerid as an empty string
 
 ### Function Script ###
 
 def save_text():
+    now = datetime.datetime.now() #setsnow as the current date and time when function is called
+    current_time = now.strftime("%H:%M:%S") #sets the formate for the current time
     notes_string = notes.get("1.0", "end-1c") # converts the text from notes into a string that can be saved to file.
     callerid = caller_answer.get() # gets the string value of the caller entry to save to file.
     with open(file_title, 'a',) as file: # opens file ready to be added to (a for append) and creates file if it doesn't exist.
-        file.write(f"### {current_time} ###\n{callerid}\n{notes_string}\n") # stamps current time, line break(lb) stamps caller ID, lb adds the call notes to the txt file
+        file.write(f"\n### {current_time} ###\n{callerid}\n{notes_string}\n") # stamps current time, line break(lb) stamps caller ID, lb adds the call notes to the txt file
         caller_answer.delete(0, tk.END) # deletes the text in caller ID box ready for next call
         notes.delete("1.0", tk.END) # deletes the text in notes box ready for next call
         file.close() # closes the file when we are finished.
 
 ### GUI layout ###
 
-root.columnconfigure(0, weight=1)
+root.columnconfigure(0, weight=1) 
 root.rowconfigure(0, weight=1)
 
 frame = tk.Frame(root, bg="#09330d")
@@ -61,8 +54,8 @@ caller_answer.grid(row= 0, column= 1, sticky="nsew", pady=5, padx= 10)
 
 notes = tk.Text(root,  height=8, bg="#f7f7e8", fg="#09330d", font=font)
 notes.grid(row= 1, column= 0, columnspan=2, sticky="nsew")
-notes_string = notes.get("1.0", "end-1c")
 
 submit_btn = tk.Button(root, text="End Call", font=font, bg="#09330D", fg="#9cbda1", command=save_text )
+submit_btn.place(rely=1.0, relx=1.0, anchor=tk.SE)
 
 root.mainloop()
--- README.txt
+++ README.txt
@@ -3,3 +3,12 @@ First python Project
 Concept is a lightweight note app that will allow me to enter a contacts name when they call, fill a textbox with notes from the call and  submit them as a timestamped entry into a .txt that will be created and named based on the date.
 
 I've been learning fundementals from 'learn python the hard way' as well as searching online and playing around with scripts, far from a pro and any constructive feedback is appreciated.
+
+################################
+
+A tool developed for streamlined my call answer process with work.
+
+1. add the dist folder to a directory you will remember and open the application.
+2. fill the box next to 'Caller ->' with the name of the caller.
+3. fill any notes from the call in the large text box.
+4. press the 'end call' button to save the time stamp, caller ID and notes to a text file in the set directory named after the current date.
